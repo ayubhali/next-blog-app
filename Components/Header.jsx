5575c5240147a9b3d27482050796980c4560b33f
Ayub Ali <pltsummary@gmail.com>
2025-02-24 21:08:37 -0700

    header component completed

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -4,14 +4,43 @@ import { assets } from "@/Assets/assets";
 
 const Header = () => {
   return (
-    <div className="py-5 px-5 md:px-12 lg:px-20">
-      <div className="flex justify-between items-center"></div>
-      <Image
-        src={assets.logo}
-        width={180}
-        alt="logo"
-        className="w-[130px] sm:w-auto"
-      />
+    <div className="py-5 px-5 md:px-12 lg:px-28">
+      <div className="flex justify-between items-center">
+        <Image
+          src={assets.logo}
+          width={180}
+          alt="logo"
+          className="w-[130px] sm:w-auto"
+        />
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
+          Get Started <Image src={assets.arrow} alt="arrow" />
+        </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <form
+          className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]"
+          action=""
+        >
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="pl-4 outline-none"
+          />
+          <button
+            type="submit"
+            className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
