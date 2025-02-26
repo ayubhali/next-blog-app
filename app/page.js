ca0ee7d74d5d094b05f7cac8ac1f08f83b78388a
Ayub Ali <pltsummary@gmail.com>
2025-02-26 14:49:23 -0700

    footer

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 "use client";
 import Header from "@/Components/Header";
 import BlogItem from "@/Components/BlogItem";
+import Bloglist from "@/Components/Bloglist";
+import Footer from "@/Components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <BlogItem />
+      <Bloglist />
+      <Footer />
     </>
   );
 }
