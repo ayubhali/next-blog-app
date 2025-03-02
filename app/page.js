3f6e48b942c1a9963dfe0aab3f3cf2cdc64918a7
Ayub Ali <pltsummary@gmail.com>
2025-03-02 14:34:45 -0700

    Fix build issues and add Dockerfile

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
-import BlogList from 'Components/BlogList.jsx';
-import Footer from 'Components/Footer.jsx';
-import Header from 'Components/Header.jsx';
+import BlogList from './components/BlogList.jsx';
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
