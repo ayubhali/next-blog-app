b2c873dfc452f4093950c776a3e6348b4db752a7
Ayub Ali <pltsummary@gmail.com>
2025-03-02 14:38:31 -0700

    update

--- app/components/BlogList.jsx
+++ app/components/BlogList.jsx
@@ -1,34 +1,36 @@
-import { blog_data } from '../Assets/assets'
+'use client'
 import React, { useEffect, useState } from 'react'
 import BlogItem from './BlogItem'
-import axios from 'axios';
+import axios from 'axios'
 
 const BlogList = () => {
+  const [blogs, setBlogs] = useState([])
 
-    const [menu,setMenu] = useState("All");
-    const [blogs,setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog')
+    setBlogs(response.data.blogs)
+  }
 
-    const fetchBlogs = async () =>{
-      const response = await axios.get('/api/blog');
-      setBlogs(response.data.blogs);
-      console.log(response.data.blogs);
-    }
-
-    useEffect(()=>{
-      fetchBlogs();
-    },[])
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
 
   return (
-    <div>
-      <div className='flex justify-center gap-6 my-10'>
-        <button onClick={()=>setMenu('All')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm':""}>All</button>
-        <button onClick={()=>setMenu('Technology')} className={menu==="Technology"?'bg-black text-white py-1 px-4 rounded-sm':""}>Technology</button>
-        <button onClick={()=>setMenu('Startup')} className={menu==="Startup"?'bg-black text-white py-1 px-4 rounded-sm':""}>Startup</button>
-        <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"?'bg-black text-white py-1 px-4 rounded-sm':""}>Lifestyle</button>
-      </div>
-      <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-        {blogs.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
-            return <BlogItem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category} />
+    <div className='flex-1 py-5 px-5 sm:py-12 sm:px-16'>
+      <div className='flex flex-wrap gap-8'>
+        {blogs.map((item, index) => {
+          return (
+            <BlogItem
+              key={index}
+              mongoId={item._id}
+              title={item.title}
+              desc={item.description}
+              author={item.author}
+              authorImg={item.authorImg}
+              date={item.date}
+              coverImg={item.image}
+            />
+          )
         })}
       </div>
     </div>
