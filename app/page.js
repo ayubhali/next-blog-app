de4fd36d9e4fda3fc2e7c2936d7c05df3d659a55
Ayub Ali <pltsummary@gmail.com>
2025-03-02 14:30:49 -0700

    railway redploy error

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
-import BlogList from "../Components/BlogList.jsx";
-import Footer from "../Components/Footer.jsx";
-import Header from "../Components/Header.jsx";
+import BlogList from 'Components/BlogList.jsx';
+import Footer from 'Components/Footer.jsx';
+import Header from 'Components/Header.jsx';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
